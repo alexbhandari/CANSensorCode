aaab1d2155791217b5a1cb7645195f686c525a80
Alex Bhandari-Young <alexbhandari@gmail.com>
2016-01-31 01:06:07 -0500

    Git init. CAN and ADC code in progress.

---
+++ Debug/source/adc.pp
@@ -0,0 +1,29 @@
+# FIXED
+
+source/adc.obj: ../source/adc.c
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/adc.h
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/reg_adc.h
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/sys_common.h
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/hal_stdtypes.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+source/adc.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/sys_vim.h
+source/adc.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/reg_vim.h
+
+../source/adc.c: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/adc.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/reg_adc.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/sys_common.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/hal_stdtypes.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/sys_vim.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/reg_vim.h: 
---
+++ Debug/source/gio.pp
@@ -0,0 +1,29 @@
+# FIXED
+
+source/gio.obj: ../source/gio.c
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/gio.h
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/reg_gio.h
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/sys_common.h
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/hal_stdtypes.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+source/gio.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/sys_vim.h
+source/gio.obj: /Users/alexbhandari/Documents/eagle/CAN_example/include/reg_vim.h
+
+../source/gio.c: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/gio.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/reg_gio.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/sys_common.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/hal_stdtypes.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/sys_vim.h: 
+/Users/alexbhandari/Documents/eagle/CAN_example/include/reg_vim.h: 
